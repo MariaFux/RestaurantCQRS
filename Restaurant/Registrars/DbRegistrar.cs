f2abf9d1f11b067a6d6f4b73d8950842e5f311d4
мария пугачёва <mawytochka99@mail.ru>
2023-08-23 15:31:38 +0300

    add registrars

---
+++ Restaurant/Registrars/DbRegistrar.cs
@@ -0,0 +1,14 @@
+﻿using Dal;
+using Microsoft.EntityFrameworkCore;
+
+namespace Restaurant.Registrars
+{
+    public class DbRegistrar : IWebApplicationBuilderRegistrar
+    {
+        public void RegisterServices(WebApplicationBuilder builder)
+        {
+            var connectionString = builder.Configuration.GetConnectionString("RestaurantDb");
+            builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(connectionString));
+        }
+    }
+}
---
+++ Restaurant/Registrars/IWebApplicationBuilderRegistrar.cs
@@ -0,0 +1,7 @@
+﻿namespace Restaurant.Registrars
+{
+    public interface IWebApplicationBuilderRegistrar : IRegistrar
+    {
+        void RegisterServices(WebApplicationBuilder builder);
+    }
+}
